49665a4c678eb512191a3a6254ef0ce0c6fff1e1
ganeshdevnale16 <ganesh.devnale16@gmail.com>
2026-03-22 16:02:12 +0530

    Update pipeline.py

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -2,18 +2,18 @@ import subprocess
 
 def run_scraper():
     print("scrapper running")
-    subprocess.run(["python","../scraper/scrapper.py"])
+    subprocess.run(["python","scraper/scrapper.py"])
     print("complete")
 
 
 def run_clean():
-    subprocess.run(["python","../scraper/cleandata.py"])
+    subprocess.run(["python","scraper/cleandata.py"])
 
 def run_compare():
-    subprocess.run(["python","../scraper/compare.py"])
+    subprocess.run(["python","scraper/compare.py"])
 
 def run_alert():
-    subprocess.run(["python","../scraper/alert.py"])
+    subprocess.run(["python","scraper/alert.py"])
 
 def run_full_pipeline():
 
