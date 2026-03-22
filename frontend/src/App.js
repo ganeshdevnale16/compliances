fc71bbb4b80120d3ca7efbc129586da73e3fa34b
ganeshdevnale16 <ganesh.devnale16@gmail.com>
2026-03-22 15:20:13 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,62 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import Header from "./components/Header";
+
+// import Home from "./pages/Home";
+// import Dashboard from "./pages/Dashboard";
+// import Entities from "./pages/Entities";
+// import Alerts from "./pages/Alerts";
+// import Reports from "./pages/Reports";
+// import Automation from "./pages/Automation";
+// const api = axios.create({
+//   baseURL: "https://compclean.onrender.com"
+// });
+// import "./App.css";
+
+// function App() {
+
+//   return (
+
+//     <BrowserRouter>
+
+//       <Header />
+
+//       <div className="page">
+
+//         <Routes>
+
+//           <Route path="/" element={<Home />} />
+
+//           <Route path="/dashboard" element={<Dashboard />} />
+
+//           <Route path="/entities" element={<Entities />} />
+
+//           <Route path="/alerts" element={<Alerts />} />
+
+//           <Route path="/reports" element={<Reports />} />
+
+//           <Route path="/automation" element={<Automation />} />
+
+//         </Routes>
+
+//       </div>
+
+//     </BrowserRouter>
+
+//   );
+
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -12,37 +71,22 @@ import Automation from "./pages/Automation";
 import "./App.css";
 
 function App() {
-
   return (
-
     <BrowserRouter>
-
       <Header />
 
       <div className="page">
-
         <Routes>
-
           <Route path="/" element={<Home />} />
-
           <Route path="/dashboard" element={<Dashboard />} />
-
           <Route path="/entities" element={<Entities />} />
-
           <Route path="/alerts" element={<Alerts />} />
-
           <Route path="/reports" element={<Reports />} />
-
           <Route path="/automation" element={<Automation />} />
-
         </Routes>
-
       </div>
-
     </BrowserRouter>
-
   );
-
 }
 
 export default App;
