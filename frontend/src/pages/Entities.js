1d5eb5f512a94ed6baf3977b453286af301f8529
ganeshdevnale16 <ganesh.devnale16@gmail.com>
2026-03-22 20:12:39 +0530

    Update Entities.js

--- frontend/src/pages/Entities.js
+++ frontend/src/pages/Entities.js
@@ -1,63 +1,13 @@
-import { useState } from "react";
+import "./entities.css";
 
 function Entities() {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [status, setStatus] = useState("");
-
-  const runScraper = async () => {
-    setLoading(true);
-    setLogs(["Starting scraper..."]);
-    setStatus("");
-
-    try {
-      const res = await fetch("http://127.0.0.1:8000/run-scraper");
-      const data = await res.json();
-
-      // show logs
-      setLogs(data.logs || []);
-
-      // show status
-      if (data.status === "success") {
-        setStatus(`✅ Success | Records: ${data.records}`);
-      } else {
-        setStatus(`❌ Failed: ${data.error}`);
-      }
-
-    } catch (err) {
-      setStatus("❌ Error: " + err.message);
-    }
-
-    setLoading(false);
-  };
-
   return (
-    <div>
-      <h1>Entities</h1>
-
-      <button onClick={runScraper} disabled={loading}>
-        {loading ? "Running..." : "Run Scraper"}
-      </button>
-
-      {/* STATUS */}
-      <h3 style={{ marginTop: "20px" }}>{status}</h3>
-
-      {/* LOGS */}
-      <div
-        style={{
-          marginTop: "10px",
-          background: "#000",
-          color: "#0f0",
-          padding: "10px",
-          height: "300px",
-          overflowY: "auto",
-          fontFamily: "monospace"
-        }}
-      >
-        {logs.map((log, i) => (
-          <div key={i}>▶ {log}</div>
-        ))}
-      </div>
+    <div className="entities-container">
+      <img
+        src="https://thumbs.dreamstime.com/b/d-webpage-under-construction-concept-d-cartoon-characters-workers-wearing-vests-helmets-holding-jackhammer-laptop-114162972.jpg?w=992"
+        alt="Under Construction"
+        className="entities-image"
+      />
     </div>
   );
 }
